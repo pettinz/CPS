b4a7854babf326f7deb634a9c0d70f95f9c9d562
mike97-pro <68898684+mike97-pro@users.noreply.github.com>
2021-04-22 15:13:11 +0200

    Create get_RSS.m
    RSS

---
+++ Project 1/get_RSS.m
@@ -0,0 +1,7 @@
+function [RSS] = get_RSS(measured,sens,Pt,dev_stand)
+
+dist = (vecnorm((measured-sens)'))';
+d = (dist<=8);
+RSS = d.*(Pt-40.2-20.*log10(dist)+dev_stand*randn()) + (1-d).*(Pt-58.5-33.*log10(dist)+dev_stand*randn());
+end
+
