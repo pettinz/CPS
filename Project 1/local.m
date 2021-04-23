c357cc7d8ac40fa0d4369483cafdb18bae695fb6
mentz97 <68898220+mentz97@users.noreply.github.com>
2021-04-23 15:17:04 +0200

    Fino a DIST c'è

--- Project 1/local.m
+++ Project 1/local.m
@@ -74,8 +74,8 @@ ni=50; %%number of iterations
 lam = 1e-4;
 tau = 0.7;
 th = 0.5;
-max_iter = 1e5;
-min_eps = 1e-6;
+max_iter = 1e4;
+min_eps = 1e-5;
 success = 0;
 iter=zeros(ni, 1);
 dist=zeros(ni, 1);
@@ -104,29 +104,19 @@ for i=1:ni
         Ap=A;
     end
     
-    xt_1 = zeros(p,1);
+    [xt, iter(i)]=ist(max_iter, tau, Ap, yp, min_eps, lam);
     
-    for j=1:max_iter    
-        xt = soft_tresh(xt_1+tau.*(Ap'*(yp-Ap*xt_1)), lam);
-        eps = norm(xt-xt_1,2);
-        
-        if eps <= min_eps 
-            break
-        end
-        xt_1 = xt;
-    end
-  
     [~, p_cell] = max(abs(xt));
     p_cell=p_cell-1;
     x_estimated=fix(p_cell/10)+l_p/2;
     y_estimated=mod(p_cell, 10)+l_p/2;
     dist(i)= norm([x_estimated y_estimated] - [x_measured y_measured]);
-    iter(i)=j;
+ 
     if cell(i)==p_cell+1
-        fprintf('Success\nnum iter: %d\n',j);
+        fprintf('Success\n');
         success=success+1;
     else
-        fprintf('Fail\nnum iter: %d\n', j);
+        fprintf('Fail\n');
     end
     fprintf('Cella: %d, Predetta: %d\n', cell(i), p_cell+1);
     figure(3), p2=plot(x_estimated, y_estimated, '.k', 'MarkerSize', 10);
@@ -139,21 +129,37 @@ fprintf('\n\nSuccess rate: %2.0f%%\nAverage number of iterations: %d\n',...
     (success/ni*100), round(mean(iter)));
 figure()
 plot([1:ni], dist, '--*')
-xlabel('iterations')
+xlabel('iteration')
 ylabel('distance(m)')
 title('IST')
+xlim([1 50])
+pause()
+
+figure()
+plot([1:ni], iter, '--*')
+hold on 
+plot([1 ni], [mean(iter) mean(iter)], '--r')
+xlabel('iteration')
+ylabel('number of iterations')
+title('IST')
+ylim([min(iter) max(iter)])
+xlim([1 50])
+legend('number of iterations', 'average number of iterations', 'Location', 'southwest')
+legend('boxoff')
+pause()
 
 %% DIST
 figure(6)
 make_grid(xg, yg, x_sens, y_sens);
+title('DIST')
 hold on
 
-lam = 1e-5;
+lam = 1e-4;
 tau = 0.7;
-th = 0.5;
-max_iter = 1e5;
-min_eps = 1e-5;
+max_iter = 5e4;
+min_eps = 1e-6;
 success = 0;
+iter=zeros(ni,1);
 
 for it=1:ni
     x_measured=x_ref(cell(it));
@@ -172,40 +178,21 @@ for it=1:ni
         yp=y;
         Ap=A;
     end
-        
-    xt_0 = zeros(p,n);
-    done=zeros(n,1);
-    iter=zeros(n,1);
     
-    for k=1:max_iter
-        for i=1:n
-            if done(i)==1
-                continue;
-            end
-            iter(i)=iter(i)+1;
-            xt(:,i) = soft_tresh(xt_0*Q(i,:)'+tau.*(Ap(i,:)'*(yp(i)-Ap(i,:)*xt_0(:,i))), lam);
-            eps = norm(xt(:,i)-xt_0(:,i),2);
-            if eps <= min_eps
-                done(i)=1;
-            end
-        end
-        if(min(done)==1)
-            break;
-        end
-        xt_0=xt;
-    end
+    [xt, iter(it)]=distt(Ap, yp, max_iter, Q, tau, lam, min_eps); 
     %round(xt,2) %% see if consensus
     
     [~, p_cell] = max(abs(xt));
     p_cell=p_cell-1;
     x_estimated=fix(p_cell/10)+l_p/2;
     y_estimated=mod(p_cell, 10)+l_p/2;
-    
+    dist(it)= norm([mode(x_estimated) mode(y_estimated)] - [x_measured y_measured]);
+ 
     if cell(it)==mode(p_cell+1)
-        fprintf('Success\nnum iter: %d\n', round(mean(iter)));
+        fprintf('Success\nnum iter: %d\n', iter(it));
         success=success+1;
     else
-        fprintf('Fail\nnum iter: %d\n', round(mean(iter)));
+        fprintf('Fail\nnum iter: %d\n', iter(it));
     end
     fprintf('Cella: %d, Predetta: %d\n', cell(it), mode(p_cell+1));
     figure(6), p2=plot(x_estimated, y_estimated, '.k', 'MarkerSize', 10);
@@ -214,4 +201,23 @@ for it=1:ni
     delete(p1)
     delete(p2)
 end
-fprintf('Success rate: %2.0f%%\n',(success/ni*100));
+fprintf('\n\nSuccess rate: %2.0f%%\nAverage number of iterations: %d\n',...
+    (success/ni*100), round(mean(iter)));
+figure()
+plot([1:ni], dist, '--*')
+xlabel('iteration')
+ylabel('distance(m)')
+title('DIST')
+pause()
+
+figure()
+plot([1:ni], iter, '--*')
+hold on 
+plot([1 ni], [mean(iter) mean(iter)], '--r')
+xlabel('iteration')
+ylabel('number of iterations')
+title('DIST')
+ylim([min(iter) max(iter)])
+xlim([1 50])
+legend('number of iterations', 'average number of iterations', 'Location', 'southwest')
+legend('boxoff')
